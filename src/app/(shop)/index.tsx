f20a823a77b287a3195cf1e18da4badeda2dce29
Ayomide Akintan <emzyakints2005@gmail.com>
2024-11-04 09:34:07 +0100

    working on flatlist header for product list

--- src/app/(shop)/index.tsx
+++ src/app/(shop)/index.tsx
@@ -1,15 +1,17 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import { PRODUCTS } from "../../../assets/products";
+import { ProductListItem } from "../../components/product-list-item";
+import { ListHeader } from "../../components/list-header";
 
 const Home = () => {
     return (
         <View>
             <FlatList
                 data={PRODUCTS}
-                renderItem={({ item }) => <View></View>}
+                renderItem={({ item }) => <ProductListItem product={item} />}
                 keyExtractor={(item) => item.id.toString()}
                 numColumns={2}
-                ListHeaderComponent={<Text>Products</Text>}
+                ListHeaderComponent={ListHeader}
                 contentContainerStyle={styles.flatListContent}
                 columnWrapperStyle={styles.flatListColumn}
                 style={{ paddingHorizontal: 10, paddingVertical: 5 }}
@@ -25,6 +27,6 @@ const styles = StyleSheet.create({
         paddingBottom: 20,
     },
     flatListColumn: {
-        justifyContent: 'space-between'
+        justifyContent: "space-between",
     },
 });
